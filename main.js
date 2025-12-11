ac9e6518da3c2b45e08aae9e5e66ec0e1b0f39c8
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2025-12-11 09:27:29 +0100

    restart btn and google fonts

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const CELL_SIZE = 20; //varje ruta på spelplanen är 20x20px, snake moves one square at time.
 const GRID_SIZE = 25; //25x25 squares
 
+const restartBtn = document.getElementById("restart");
 //Where the snake starts.
 let snake = {
   x: 12, //column 12
@@ -25,10 +26,25 @@ function update() {
   // Update snake position based on direction
   snake.x += direction.x;
   snake.y += direction.y;
+
+  // Checkout if the snake goes outside the board
+  if (
+    snake.x < 0 ||
+    snake.x >= GRID_SIZE ||
+    snake.y < 0 ||
+    snake.y >= GRID_SIZE
+  ) {
+    snake.x = 12;
+    snake.y = 12;
+    console.log("BOOM! The snake hit the wall!");
+  }
 }
+let isGameRunning = false;
 
 // Main game loop
 function gameLoop() {
+  if (!isGameRunning) return; // Stops if its not running
+
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
 
   update(); // Update snake position
@@ -36,6 +52,22 @@ function gameLoop() {
 
   setTimeout(gameLoop, 200); // Call gameLoop every 200ms and go to next one.
 }
-gameLoop();
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" && !isGameRunning) {
+    isGameRunning = true;
+    console.log("Game started with SPACE!");
+    gameLoop();
+  }
+});
+
+restartBtn.onclick = () => {
+  game.stop();
+  input.stop();
+  startGame();
+};
+
+drawSnake();
+console.log("Press SPACE to start!");
 
 console.log("Game started");
